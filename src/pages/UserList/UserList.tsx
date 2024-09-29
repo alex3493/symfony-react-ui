b98c1db3ee84ef97b5c15d0e4e3cfd12d8def371
Alex <admin@smartcalc.org>
2024-09-29 20:22:21 +0200

    Remove unused code.

--- src/pages/UserList/UserList.tsx
+++ src/pages/UserList/UserList.tsx
@@ -33,41 +33,6 @@ function UserList() {
         // mapper={mapper}
         mapper={(data: UserModel) => new UserModel(data)}
       />
-
-      {/*<Table striped>*/}
-      {/*  <thead>*/}
-      {/*    <tr>*/}
-      {/*      <SortableHeader*/}
-      {/*        isOrderDesc={isOrderDesc}*/}
-      {/*        isOrderedBy={isOrderedBy}*/}
-      {/*        onClick={onColumnHeaderClick}*/}
-      {/*        label="Name"*/}
-      {/*        column="name"*/}
-      {/*      />*/}
-      {/*      <SortableHeader*/}
-      {/*        isOrderDesc={isOrderDesc}*/}
-      {/*        isOrderedBy={isOrderedBy}*/}
-      {/*        onClick={onColumnHeaderClick}*/}
-      {/*        label="Email"*/}
-      {/*        column="email"*/}
-      {/*      />*/}
-      {/*    </tr>*/}
-      {/*  </thead>*/}
-      {/*  <tbody>*/}
-      {/*    {items?.length > 0 ? (*/}
-      {/*      items.map((user: UserModel) => (*/}
-      {/*        <tr key={user.id}>*/}
-      {/*          <td>{user.display_name}</td>*/}
-      {/*          <td>{user.email}</td>*/}
-      {/*        </tr>*/}
-      {/*      ))*/}
-      {/*    ) : (*/}
-      {/*      <tr>*/}
-      {/*        <td colSpan={2}>{dataLoaded ? 'No users found' : <Loader />}</td>*/}
-      {/*      </tr>*/}
-      {/*    )}*/}
-      {/*  </tbody>*/}
-      {/*</Table>*/}
     </div>
   )
 }
